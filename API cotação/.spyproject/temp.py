770d217e2dae31932da62b935439f8a125c8bfbd
Junio Carvalho <junioocarvalho@gmail.com>
2026-02-10 17:37:38 -0300

    Primeiro commit do projeto de cotação PTAX

---
+++ API cotação/.spyproject/temp.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import requests
+
+url_base = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata"
+
+endpoint = (
+    "/CotacaoDolarDia(dataCotacao=@dataCotacao)"
+    "?@dataCotacao='08-02-2025'&$format=json"
+)
+
+response = requests.get(url_base + endpoint)
+
+if response.status_code == 200:
+    dados = response.json()
+    print(dados)
+else:
+    print("Erro:", response.status_code, response.text)
+    
+from datetime import datetime, timedelta
+
+def cotacao_dolar_segura(data):
+    data_str = data.strftime("%m-%d-%Y")
+
+    endpoint = (
+        "/CotacaoDolarDia(dataCotacao=@dataCotacao)"
+        f"?@dataCotacao='{data_str}'&$format=json"
+    )
+
+    r = requests.get(url_base + endpoint)
+    dados = r.json()
+
+    if dados["value"]:
+        return dados["value"][0]
+    else:
+        return None
+    
+dados = response.json()
+resultado = dados.get("value", [])
+
+if resultado:
+    print(resultado[0])
+else:
+    print("Não há cotação disponível para essa data.")
+
---
+++ aula 1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  4 14:43:42 2026
+
+@author: junio
+"""
+# Script de hoje
+
+1+1
